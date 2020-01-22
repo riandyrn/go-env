2752412f3719bd4f0bcdb49c9c0d021b05e42939
Riandy Rahman Nugraha <riandyrn@gmail.com>
2020-01-22 11:12:05 +0700

    add test for GetSeconds() & GetStrings();

--- env_test.go
+++ env_test.go
@@ -2,7 +2,9 @@ package env
 
 import (
 	"os"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestGetString(t *testing.T) {
@@ -147,6 +149,114 @@ func TestGetBool(t *testing.T) {
 			val := GetBool(testCase.EnvKey)
 			if val != testCase.ExpValue {
 				t.Fatalf("unexpected val, expected: %v, got: %v", testCase.ExpValue, val)
+			}
+		})
+	}
+}
+
+func TestGetSeconds(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		EnvKey   string
+		EnvValue string
+		ExpValue time.Duration
+	}{
+		{
+			Name:     "Test Key Not Set",
+			EnvKey:   "",
+			EnvValue: "123",
+			ExpValue: 0 * time.Second,
+		},
+		{
+			Name:     "Test Value Empty",
+			EnvKey:   "Key",
+			EnvValue: "",
+			ExpValue: 0 * time.Second,
+		},
+		{
+			Name:     "Test Value Not Integer (String)",
+			EnvKey:   "Key",
+			EnvValue: "ABC",
+			ExpValue: 0 * time.Second,
+		},
+		{
+			Name:     "Test Value Not Integer (Float)",
+			EnvKey:   "Key",
+			EnvValue: "123.132",
+			ExpValue: 0 * time.Second,
+		},
+		{
+			Name:     "Test Value Integer",
+			EnvKey:   "Key",
+			EnvValue: "60",
+			ExpValue: 60 * time.Second,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			if len(testCase.EnvKey) != 0 {
+				os.Setenv(testCase.EnvKey, testCase.EnvValue)
+			}
+			val := GetSeconds(testCase.EnvKey)
+			if val != testCase.ExpValue {
+				t.Fatalf("unexpected val, expected: %v, got: %v", testCase.ExpValue, val)
+			}
+		})
+	}
+}
+
+func TestGetStrings(t *testing.T) {
+	testCases := []struct {
+		Name      string
+		EnvKey    string
+		EnvValue  string
+		Separator string
+		ExpValue  []string
+	}{
+		{
+			Name:      "Test Key Not Set",
+			EnvKey:    "",
+			EnvValue:  "ABC",
+			Separator: ",",
+			ExpValue:  nil,
+		},
+		{
+			Name:      "Test Value Empty",
+			EnvKey:    "Key",
+			EnvValue:  "",
+			Separator: ",",
+			ExpValue:  nil,
+		},
+		{
+			Name:      "Test Value Not Empty - Single Item",
+			EnvKey:    "Key",
+			EnvValue:  "ABC",
+			Separator: ",",
+			ExpValue:  []string{"ABC"},
+		},
+		{
+			Name:      "Test Value Not Empty - Mulitple Items",
+			EnvKey:    "Key",
+			EnvValue:  "ABC,DEF,GHI",
+			Separator: ",",
+			ExpValue:  []string{"ABC", "DEF", "GHI"},
+		},
+		{
+			Name:      "Test Value Not Empty - Mulitple Items With Space",
+			EnvKey:    "Key",
+			EnvValue:  "ABC, DEF, G HI",
+			Separator: ", ",
+			ExpValue:  []string{"ABC", "DEF", "G HI"},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			if len(testCase.EnvKey) != 0 {
+				os.Setenv(testCase.EnvKey, testCase.EnvValue)
+			}
+			val := GetStrings(testCase.EnvKey, testCase.Separator)
+			if strings.Join(val, testCase.Separator) != strings.Join(testCase.ExpValue, testCase.Separator) {
+				t.Fatalf("unexpected val, expected: %v, got: %v", testCase.EnvValue, val)
 			}
 		})
 	}
